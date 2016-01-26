b09231dc02d0a4af59eabb45f6d9eed858dc67c2
iCHAIT <cgupta319@gmail.com>
2016-01-26 19:14:05 +0530

    description added

--- arrays/min-dis.c
+++ arrays/min-dis.c
@@ -49,3 +49,8 @@ int main()
               minDist(arr, n, x, y));
     return 0;
 }
+
+/*
+Find the minimum distance between two numbers
+Given an unsorted array arr[] and two numbers x and y, find the minimum distance between x and y in arr[]. The array might also contain duplicates. You may assume that both x and y are different and present in arr[].
+*/
