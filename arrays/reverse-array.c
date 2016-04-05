0f63f47f353ffc7885f298aba962cf3f148e61f0
iCHAIT <cgupta319@gmail.com>
2016-04-05 23:09:49 +0530

    reverse an array

---
+++ arrays/reverse-array.c
@@ -0,0 +1,37 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+
+int main()
+{
+
+	int arr[] = {3,4,1,2,6,5};
+
+	int size = (sizeof(arr))/(sizeof(arr[0]));
+	int start=0, end = size-1,i,temp=0;
+
+	printf("Input Array is");
+
+	for (i=0;i<size;i++)
+		printf("%d ", arr[i]);
+
+	i=0;
+
+	while(i<size/2)
+	{
+
+		temp = arr[start];
+		arr[start] = arr[end];
+		arr[end] = temp;
+		start++;
+		end--;
+		i++;
+
+	}
+
+printf("OUTPUT Array is");
+		for (int i=0;i<size;i++)
+		printf("%d ", arr[i]);
+
+
+}
