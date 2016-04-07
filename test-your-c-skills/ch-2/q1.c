c058db493767c285473e43f48d9178693cbd05a6
iCHAIT <cgupta319@gmail.com>
2016-04-07 22:58:44 +0530

    organized test your c skills

---
+++ test-your-c-skills/ch-2/q1.c
@@ -0,0 +1,29 @@
+# include <stdio.h>
+
+int main()
+{
+
+	int i=4;
+
+	switch(i)
+	{
+
+		default:
+			printf("\ndefault\n");
+
+		case 1:
+			printf("\ncase 1\n");
+			break;
+
+		case 2:
+			printf("\ncase 2\n");
+			break;
+
+		case 3:
+			printf("\ncase 3\n");
+
+	}
+
+	return 0;
+
+}
---
+++ test-your-c-skills/ch-2/q2.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+
+int main()
+{
+
+	int i=1;
+
+	for (; ;)
+	{
+		/* code */
+		printf("%d\n",i++);
+		if (i>10)
+			break;
+
+	}
+
+
+	return 0;
+
+}
