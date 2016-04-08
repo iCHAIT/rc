d4a5a95080ad41ebb38371336627d9149a6433ab
iCHAIT <cgupta319@gmail.com>
2016-04-08 19:55:28 +0530

    minor changes

--- bit-algo/comp-2no.c
+++ bit-algo/comp-2no.c
@@ -1,10 +1,11 @@
 # include <stdio.h>
+#define CHAR_BIT 8
 
 int max(int a, int b)
 {
 
 	int c = a-b;
-	int k = c>>31 & 0x1;
+	int k = c>>(sizeof(int)*CHAR_BIT-1) & 0x1;
 	int max = a-k*c;
 	return max;
 
