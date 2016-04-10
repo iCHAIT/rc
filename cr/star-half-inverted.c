9a62798dc6657296ba51fcdccd040fcecb119175
iCHAIT <cgupta319@gmail.com>
2016-04-10 22:13:24 +0530

    start pattern formation

---
+++ cr/star-half-inverted.c
@@ -0,0 +1,28 @@
+/*
+* * * * *
+* * * *
+* * *
+* *
+*
+*/
+
+
+# include <stdio.h>
+
+int main()
+{
+	int row=5,i,j;
+
+	for(i=row;i>=1;i--)
+	{
+
+		for(j=1;j<=i;j++)
+			printf("* ");
+
+		printf("\n");
+	}
+
+	return 0;
+
+
+}
